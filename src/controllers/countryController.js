4d20d920782cf627aa041881cb0c6cbb940bf4c7
Nashid-k <nashidk1999@gmail.com>
2026-01-08 16:04:58 +0530

    pagination

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -2,9 +2,13 @@ import { fetchAllCountries, fetchCountryByName } from "../services/countryServic
 
 export const getCountries = async (req, res) => {
     try {
-        const countries = await fetchAllCountries();
-        res.status(200).json(countries);
+        const { page = 1, limit = 12, search = "", region = "All" } = req.query;
+
+        const result = await fetchAllCountries({ page, limit, search, region });
+
+        res.status(200).json(result);
     } catch (error) {
+        console.error("Error fetching countries:", error);
         res.status(500).json({ message: "Failed to fetch countries" });
     }
 };
@@ -18,6 +22,7 @@ export const getCountryByName = async (req, res) => {
         if (error.response?.status === 404) {
             res.status(404).json({ message: "Country not found" });
         } else {
+            console.error("Error fetching country details:", error);
             res.status(500).json({ message: "Failed to fetch country" });
         }
     }
